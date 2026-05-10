2e155072120776eaf6a64105216d13d311ca2f16
Carille Pabiona <carillepabiona49@gmail.com>
2026-05-11 00:07:33 +0800

    PROFILE UPDATED

---
+++ Models/UserProfileModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Users_DASM.Models
+{
+    public class UserProfileModel
+    {
+        public string FullName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Username { get; set; }
+
+        public string ContactNumber { get; set; }
+
+        public string Address { get; set; }
+
+        public string Role { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public bool TwoFactorEnabled { get; set; }
+
+        public bool EmailNotifications { get; set; }
+
+        public bool PushNotifications { get; set; }
+
+        public bool MarketingEmails { get; set; }
+
+        public bool ActivityDigest { get; set; }
+
+        public string Theme { get; set; }
+
+        public string Language { get; set; }
+
+        public string FontSize { get; set; }
+
+        public bool ProfilePublic { get; set; }
+
+        public bool ShowActivityStatus { get; set; }
+
+        public bool AllowMessages { get; set; }
+    }
+}
---
+++ Models/UserSession.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Users_DASM.Models
+{
+    public class UserSession
+    {
+        public string Id { get; set; } = "";
+
+        public string Username { get; set; } = "";
+
+        public string FullName { get; set; } = "";
+
+        public string Email { get; set; } = "";
+
+        public string ContactNumber { get; set; } = "";
+
+        public string Address { get; set; } = "";
+
+        public int RoleId { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
