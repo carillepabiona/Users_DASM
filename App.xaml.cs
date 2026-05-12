0799df1c23f513609adb284d421bd219acacddfa
Carille Pabiona <carillepabiona49@gmail.com>
2026-05-12 22:40:01 +0800

    DONE DONE

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,15 +1,98 @@
-﻿namespace Users_DASM
-{
-    public partial class App : Application
-    {
-        public App()
-        {
-            InitializeComponent();
-        }
+﻿#if WINDOWS
+using Microsoft.UI.Windowing;
+using Windows.Graphics;
+using WinRT.Interop;
+#endif
 
-        protected override Window CreateWindow(IActivationState? activationState)
+namespace Users_DASM;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    protected override Window CreateWindow(
+        IActivationState? activationState)
+    {
+        var window = new Window(new MainPage());
+
+#if WINDOWS
+
+        window.Created += (s, e) =>
         {
-            return new Window(new MainPage()) { Title = "Users_DASM" };
-        }
+            var nativeWindow =
+                window.Handler.PlatformView;
+
+            var hwnd =
+                WindowNative.GetWindowHandle(nativeWindow);
+
+            var windowId =
+                Microsoft.UI.Win32Interop
+                    .GetWindowIdFromWindow(hwnd);
+
+            var appWindow =
+                AppWindow.GetFromWindowId(windowId);
+
+            // SAVE WINDOW INSTANCE
+            AppWindowHolder.Instance.Set(appWindow);
+
+            int loginWidth = 650;
+            int loginHeight = 720;
+
+            // =========================
+            // LOGIN WINDOW SIZE
+            // =========================
+
+            appWindow.Resize(
+                new SizeInt32(
+                    loginWidth,
+                    loginHeight));
+
+            // =========================
+            // REMOVE TITLE BAR
+            // =========================
+
+            if (appWindow.Presenter
+                is OverlappedPresenter presenter)
+            {
+                presenter.IsResizable = false;
+
+                presenter.IsMaximizable = false;
+
+                presenter.IsMinimizable = false;
+
+                presenter.SetBorderAndTitleBar(
+                    false,
+                    false);
+            }
+
+            appWindow.Title = string.Empty;
+
+            // =========================
+            // CENTER WINDOW
+            // =========================
+
+            var displayArea =
+                DisplayArea.GetFromWindowId(
+                    windowId,
+                    DisplayAreaFallback.Primary);
+
+            int x =
+                (displayArea.WorkArea.Width
+                 - loginWidth) / 2;
+
+            int y =
+                (displayArea.WorkArea.Height
+                 - loginHeight) / 2;
+
+            appWindow.Move(
+                new PointInt32(x, y));
+        };
+
+#endif
+
+        return window;
     }
 }
---
+++ AppWindowHolder.cs
@@ -0,0 +1,95 @@
+﻿#if WINDOWS
+using Microsoft.UI.Windowing;
+using Windows.Graphics;
+
+namespace Users_DASM;
+
+public class AppWindowHolder
+{
+    private static AppWindowHolder? _instance;
+
+    public static AppWindowHolder Instance
+        => _instance ??= new AppWindowHolder();
+
+    public AppWindow? Window { get; private set; }
+
+    public void Set(AppWindow window)
+    {
+        Window = window;
+    }
+
+    // =========================
+    // RESTORE NORMAL APP WINDOW
+    // =========================
+    public void RestoreFullWindow()
+    {
+        if (Window == null)
+            return;
+
+        Window.SetPresenter(AppWindowPresenterKind.Overlapped);
+
+        if (Window.Presenter is OverlappedPresenter presenter)
+        {
+            presenter.IsResizable = true;
+            presenter.IsMaximizable = true;
+            presenter.IsMinimizable = true;
+
+            presenter.SetBorderAndTitleBar(true, true);
+
+            presenter.Maximize();
+        }
+
+        Window.Title = "Users DASM";
+    }
+
+    // =========================
+    // TRUE FULLSCREEN
+    // =========================
+    public void SetFullScreen()
+    {
+        if (Window == null)
+            return;
+
+        Window.SetPresenter(AppWindowPresenterKind.FullScreen);
+    }
+
+    // =========================
+    // EXIT FULLSCREEN
+    // =========================
+    public void ExitFullScreen()
+    {
+        if (Window == null)
+            return;
+
+        Window.SetPresenter(AppWindowPresenterKind.Overlapped);
+    }
+
+    // =========================
+    // RETURN TO LOGIN WINDOW
+    // =========================
+    public void RestoreLoginWindow()
+    {
+        if (Window == null)
+            return;
+
+        int loginWidth = 650;
+        int loginHeight = 720;
+
+        Window.SetPresenter(AppWindowPresenterKind.Overlapped);
+
+        Window.Resize(
+            new SizeInt32(loginWidth, loginHeight));
+
+        if (Window.Presenter is OverlappedPresenter presenter)
+        {
+            presenter.IsResizable = false;
+            presenter.IsMaximizable = false;
+            presenter.IsMinimizable = false;
+
+            presenter.SetBorderAndTitleBar(false, false);
+        }
+
+        Window.Title = string.Empty;
+    }
+}
+#endif
